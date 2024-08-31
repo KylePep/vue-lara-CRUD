86b5a91588943fdd4fecad104ac94e4d442df5fe
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-08-31 03:03:50 -0600

    debugging launch.json

---
+++ client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-wpu5c30lsr3ovcpu.us.auth0.com'
+export const clientId = 'UFzULS7I9cylFJHgxKQDn74l3NLGj1jo'
+export const audience = 'classroom.com'
